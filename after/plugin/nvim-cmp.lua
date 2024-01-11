188e7496ceb583f11b3162512ba205dc986b3fdd
stepanchous <s.kolovorotnyy@gmail.com>
2024-01-11 16:49:59 +0300

    fixed cmp window size

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -8,6 +8,28 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 vim.opt.completeopt = "menu,menuone,noselect"
 
+local format_fn = function(entry, item)
+	item.menu = ""
+	local fixed_width = 40
+	local content = item.abbr
+
+	if fixed_width then
+		vim.o.pumwidth = fixed_width
+	end
+
+	local win_width = vim.api.nvim_win_get_width(0)
+
+	local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+	if #content > max_content_width then
+		item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+	else
+		item.abbr = content .. (" "):rep(max_content_width - #content)
+	end
+
+	return item
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -22,7 +44,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	}),
 
 	sources = cmp.config.sources({
@@ -32,10 +54,11 @@ cmp.setup({
 		{ name = "path" }, -- file system paths
 	}),
 	formatting = {
+		fields = { "abbr", "kind" },
 		format = lspkind.cmp_format({
-			mode = "symbol",
-			maxwidth = 50,
+			maxwidth = 40,
 			ellipsis_char = "...",
+			before = format_fn,
 		}),
 	},
 	window = {
